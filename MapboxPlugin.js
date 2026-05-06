c3d801c44bc2c3aba3c6c31e912adc476587b069
devnandagopaljb <dev.nandagopaljb@gmail.com>
2026-05-06 21:26:54 +0530

    Add files via upload

---
+++ MapboxPlugin.js
@@ -0,0 +1,71 @@
+var exec = require('cordova/exec');
+
+var SERVICE = 'MapboxPlugin';
+
+function call(action, args) {
+  return new Promise(function (resolve, reject) {
+    exec(resolve, reject, SERVICE, action, args || []);
+  });
+}
+
+module.exports = {
+  diagnostic: function () {
+    var result = {
+      cordova: !!window.cordova,
+      service: SERVICE,
+      pluginObject: !!window.MapboxPlugin,
+      plugins: []
+    };
+
+    try {
+      var pluginList = cordova.require('cordova/plugin_list');
+      result.plugins = pluginList.map(function (plugin) {
+        return {
+          id: plugin.id,
+          pluginId: plugin.pluginId,
+          clobbers: plugin.clobbers || []
+        };
+      });
+    } catch (e) {
+      result.pluginListError = e && e.message ? e.message : String(e);
+    }
+
+    return result;
+  },
+
+  initialize: function (options) {
+    return call('initialize', [options || {}]);
+  },
+
+  ping: function () {
+    return call('ping', []);
+  },
+
+  setCamera: function (options) {
+    return call('setCamera', [options || {}]);
+  },
+
+  setViewport: function (options) {
+    return call('setViewport', [options || {}]);
+  },
+
+  addMarker: function (options) {
+    return call('addMarker', [options || {}]);
+  },
+
+  removeMarker: function (id) {
+    return call('removeMarker', [{ id: id }]);
+  },
+
+  clearMarkers: function () {
+    return call('clearMarkers', []);
+  },
+
+  getCamera: function () {
+    return call('getCamera', []);
+  },
+
+  close: function () {
+    return call('close', []);
+  }
+};
