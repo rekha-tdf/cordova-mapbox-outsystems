9af8f2508f6b4735a7ad42642d62ab53513f74f5
devnandagopaljb <dev.nandagopaljb@gmail.com>
2026-05-06 20:05:22 +0530

    Add files via upload

---
+++ hooks/android/verify-mapbox-feature.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (context) {
+  const projectRoot = context.opts.projectRoot;
+  const configPath = path.join(
+    projectRoot,
+    'platforms',
+    'android',
+    'app',
+    'src',
+    'main',
+    'res',
+    'xml',
+    'config.xml'
+  );
+
+  if (!fs.existsSync(configPath)) {
+    console.log('[MapboxPlugin] Android config.xml not found at: ' + configPath);
+    return;
+  }
+
+  const config = fs.readFileSync(configPath, 'utf8');
+  const hasService = config.indexOf('feature name="MapboxPlugin"') >= 0;
+  const hasClass = config.indexOf('com.outsystems.mapbox.MapboxPlugin') >= 0;
+
+  console.log('[MapboxPlugin] Android config.xml feature present: ' + hasService);
+  console.log('[MapboxPlugin] Android config.xml class present: ' + hasClass);
+
+  if (!hasService || !hasClass) {
+    console.log('[MapboxPlugin] Expected feature entry was not generated:');
+    console.log('<feature name="MapboxPlugin">');
+    console.log('  <param name="android-package" value="com.outsystems.mapbox.MapboxPlugin" />');
+    console.log('</feature>');
+  }
+};
